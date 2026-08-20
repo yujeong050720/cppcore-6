9f41ee872ef5934b4382e52b6dbe09dc4652b3e8
YUJEONG AN <yujeong_0720@naver.com>
2026-08-21 07:47:51 +0900

    Update report.h

--- report.h
+++ report.h
@@ -1,21 +1,129 @@
 #pragma once
 
+struct ST_DESC_INTERNATIONAL : public core::IFormatterObject
+{
+    std::tstring koKR;
+
+    void OnSync(core::IFormatter& formatter)
+    {
+        formatter.Sync(TEXT("ko-KR"), koKR);
+    }
+};
+
 struct ST_SUMMARY : public core::IFormatterObject
 {
     std::tstring strResult;
+    std::tstring strDetectName;
+    std::tstring strEngineName;
+    std::tstring strEngineVersion;
+    int nSeverity = 0;
+    std::tstring strSampleName;
+    std::tstring strSampleExt;
+    double dbTotalElapsedTime = 0.0;
+    bool bReachedFileLoopLimit = false;
+    bool bReachedUrlLoopLimit = false;
 
     void OnSync(core::IFormatter& formatter)
     {
         formatter.Sync(TEXT("Result"), strResult);
+        formatter.Sync(TEXT("DetectName"), strDetectName);
+        formatter.Sync(TEXT("EngineName"), strEngineName);
+        formatter.Sync(TEXT("EngineVersion"), strEngineVersion);
+        formatter.Sync(TEXT("Severity"), nSeverity);
+        formatter.Sync(TEXT("SampleName"), strSampleName);
+        formatter.Sync(TEXT("SampleExt"), strSampleExt);
+        formatter.Sync(TEXT("TotalElapsedTime"), dbTotalElapsedTime);
+        formatter.Sync(TEXT("ReachedFileLoopLimit"), bReachedFileLoopLimit);
+        formatter.Sync(TEXT("ReachedUrlLoopLimit"), bReachedUrlLoopLimit);
+    }
+};
+
+struct ST_FILE_TARGET_ITEM : public core::IFormatterObject
+{
+    std::tstring strTargetID;
+    std::tstring strParentID;
+    std::tstring strAbsolutePath;
+    std::tstring strFileName;
+    int nFileSize = 0;
+    std::tstring strFileEXT;
+    std::tstring strMD5;
+    std::tstring strSHA1;
+    std::tstring strSHA256;
+    std::tstring strHAS160;
+    std::tstring strFileHeaderDump;
+
+    void OnSync(core::IFormatter& formatter)
+    {
+        formatter.Sync(TEXT("TargetID"), strTargetID);
+        formatter.Sync(TEXT("ParentID"), strParentID);
+        formatter.Sync(TEXT("AbsolutePath"), strAbsolutePath);
+        formatter.Sync(TEXT("FileName"), strFileName);
+        formatter.Sync(TEXT("FileSize"), nFileSize);
+        formatter.Sync(TEXT("FileEXT"), strFileEXT);
+        formatter.Sync(TEXT("MD5"), strMD5);
+        formatter.Sync(TEXT("SHA1"), strSHA1);
+        formatter.Sync(TEXT("SHA256"), strSHA256);
+        formatter.Sync(TEXT("HAS160"), strHAS160);
+        formatter.Sync(TEXT("FileHeaderDump"), strFileHeaderDump);
+    }
+};
+
+struct ST_FILE_TARGET : public core::IFormatterObject
+{
+    ST_FILE_TARGET_ITEM invoice_doc_16880;
+    ST_FILE_TARGET_ITEM skeml_lnk_258;
+
+    void OnSync(core::IFormatter& formatter)
+    {
+        formatter.Sync(TEXT("invoice.doc_16880"), invoice_doc_16880);
+        formatter.Sync(TEXT("skeml.lnk_258"), skeml_lnk_258);
+    }
+};
+
+struct ST_DETECTION_EVENT : public core::IFormatterObject
+{
+    std::tstring strEngineName;
+    std::tstring strEngineVersion;
+    std::tstring strTargetID;
+    std::tstring strName;
+    std::tstring strAnalysisCode;
+    int nSeverity = 0;
+    std::tstring strDesc;
+    ST_DESC_INTERNATIONAL DescInternational;
+
+    void OnSync(core::IFormatter& formatter)
+    {
+        formatter.Sync(TEXT("EngineName"), strEngineName);
+        formatter.Sync(TEXT("EngineVersion"), strEngineVersion);
+        formatter.Sync(TEXT("TargetID"), strTargetID);
+        formatter.Sync(TEXT("Name"), strName);
+        formatter.Sync(TEXT("AnalysisCode"), strAnalysisCode);
+        formatter.Sync(TEXT("Severity"), nSeverity);
+        formatter.Sync(TEXT("Desc"), strDesc);
+        formatter.Sync(TEXT("DescInternational"), DescInternational);
+    }
+};
+
+struct ST_DETECTION : public core::IFormatterObject
+{
+    std::vector<ST_DETECTION_EVENT> Event;
+
+    void OnSync(core::IFormatter& formatter)
+    {
+        formatter.Sync(TEXT("Event"), Event);
     }
 };
 
 struct ST_ANALYSIS_REPORT : public core::IFormatterObject
 {
     ST_SUMMARY Summary;
+    ST_FILE_TARGET FileTarget;
+    ST_DETECTION Detection;
 
     void OnSync(core::IFormatter& formatter)
     {
         formatter.Sync(TEXT("Summary"), Summary);
+        formatter.Sync(TEXT("FileTarget"), FileTarget);
+        formatter.Sync(TEXT("Detection"), Detection);
     }
 };
