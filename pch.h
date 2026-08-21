1ac43e0290ad32216a8e58315cca101d300a84a9
YUJEONG AN <yujeong_0720@naver.com>
2026-08-21 12:42:07 +0900

    Update include paths for cppcore headers and library

--- pch.h
+++ pch.h
@@ -1,8 +1,9 @@
+// pch.h
 #pragma once
 
-#include "../../../cppcore/Inc/cppcore.h"
+#include "C:/Users/ido31/OneDrive/Desktop/cppcore/Inc/cppcore.h"
 #include "report.h"
 
 using namespace core;
 
-#pragma comment(lib, "cppcore.lib")
+#pragma comment(lib, "C:/Users/ido31/OneDrive/Desktop/cppcore/Build/x64Debug/cppcore.lib")
