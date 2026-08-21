00239d316c7f864542c31b2ac444f17b9e595c3a
YUJEONG AN <yujeong_0720@naver.com>
2026-08-21 12:42:54 +0900

    Modify file paths with user-specific comments
    Updated file paths for report and target XML to include comments for user modification.

--- main.cpp
+++ main.cpp
@@ -1,3 +1,4 @@
+// main.cpp
 #include "pch.h"
 #include "report.h"
 
@@ -5,9 +6,9 @@ int main(void)
 {
     setlocale(LC_ALL, "korean");
 
-    LPCTSTR pszReportJson = TEXT("C:/Users/ido31/Downloads/시스템 프로그래밍6 - 데이터 정복하기 - 실습/report.json"); 
-    LPCTSTR pszReportXml = TEXT("C:/Users/ido31/Downloads/시스템 프로그래밍6 - 데이터 정복하기 - 실습/report.xml");   
-    LPCTSTR pszTargetXml = TEXT("C:/Users/ido31/Downloads/시스템 프로그래밍6 - 데이터 정복하기 - 실습/target.xml");   
+    LPCTSTR pszReportJson = TEXT("C:/Users/ido31/Downloads/시스템 프로그래밍6 - 데이터 정복하기 - 실습/report.json"); // 본인 경로에 맞게 수정1
+    LPCTSTR pszReportXml = TEXT("C:/Users/ido31/Downloads/시스템 프로그래밍6 - 데이터 정복하기 - 실습/report.xml");   // 본인 경로에 맞게 수정2
+    LPCTSTR pszTargetXml = TEXT("C:/Users/ido31/Downloads/시스템 프로그래밍6 - 데이터 정복하기 - 실습/target.xml");   // 본인 경로에 맞게 수정3
 
     try
     {
@@ -15,23 +16,17 @@ int main(void)
 
         if (!UTF8::ReadJsonFromFile(&report, pszReportJson) ||
             !UTF8::WriteXmlToFile(&report, pszReportXml))
-        {
             throw exception_format(TEXT("Writing %s failure"), pszReportXml);
-        }
 
         std::tstring strMyXml;
         std::tstring strTargetXml;
 
         if (EC_SUCCESS != ReadFileContents(pszReportXml, strMyXml) ||
             EC_SUCCESS != ReadFileContents(pszTargetXml, strTargetXml))
-        {
             throw exception_format(TEXT("Reading TargetXml %s failure"), pszTargetXml);
-        }
 
         if (Trim(strMyXml) != Trim(strTargetXml))
-        {
             throw exception_format(TEXT("It doesn't match exactly!"));
-        }
 
         printf("You succeeded!\n");
     }
